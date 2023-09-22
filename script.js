af2929081e5385e104bacf46d3d7bf6a22211c4f
Jennifer Sence <sencejenni@gmail.com>
2023-09-22 12:27:28 +0100

    dice animation

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonEl = document.getElementById("roll-button");
+const diceEl = document.getElementById("dice");
+
+buttonEl.addEventListener("click", ()=>{
+    diceEl.classList.add("roll-animation");
+});
